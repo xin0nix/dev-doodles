a6faea05bdab2acf1168ef2550739f0995a3412c
xin0nix <xin0nix@yahoo.com>
2026-06-10 16:45:11 +0300

    03-mmap-write-unmap: scaffolding

--- dance-of-processes/03-mmap-write-unmap/NOTES.md
+++ dance-of-processes/03-mmap-write-unmap/NOTES.md
@@ -1,1 +1,16 @@
+MAN: the kernel picks a new address that may or may not depend on the hint
+OBS:
+UNK: why a buffer for the mapping is not provided by user (like in recv)?
+
+MAN: After the mmap() call has returned, the file descriptor, fd, can be closed immediately without invalidating the mapping.
+OBS:
+UNK: I assume the reason is that the target shared memory object is managed by its name via shm_open and shm_unlink; What makes this API odd to me is that I would expect something like shm_link + open/close and shm_unlink; What is the reasoning behind the design choice here?
+
+MAN: The region is automatically unmapped when the process is terminated.
+OBS:
+UNK:
+
+MAN: SEE ALSO ftruncate(2), getpagesize(2), memfd_create(2), mincore(2), mlock(2),  mmap2(2),  mprotect(2),  mremap(2),  msync(2),  remap_file_pages(2),  setrlimit(2),  shmat(2),  userfaultfd(2)
+OBS: I feel overwhelmed!
+UNK: everything here
 
--- dance-of-processes/03-mmap-write-unmap/main.c
+++ dance-of-processes/03-mmap-write-unmap/main.c
@@ -1,6 +1,8 @@
+#include <assert.h>
 #include <fcntl.h> /* For O_* constants */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h> /* For mode constants */
 #include <sys/wait.h>
@@ -9,15 +11,60 @@
 #define defer _Defer
 
 typedef struct {
+  int fd;
+  char *data;
+  const uint size;
+} MemMap;
+
+bool init_MemMap(MemMap *obj);
+bool deinit_MemMap(MemMap *obj);
+
+typedef struct {
   const char *name;
   const uint size;
   int fd;
 } ShmObj;
 
-bool deinit(ShmObj *obj);
+bool deinit_ShmObj(ShmObj *obj);
+bool init_ShmObj(ShmObj *obj);
 
-bool init(ShmObj *obj) {
-  puts("setup");
+#define init(x) _Generic((x), MemMap *: init_MemMap, ShmObj *: init_ShmObj)(x)
+#define deinit(x)                                                              \
+  _Generic((x), MemMap *: deinit_MemMap, ShmObj *: deinit_ShmObj)(x)
+
+bool init_MemMap(MemMap *obj) {
+  puts("init memory map");
+  auto buf =
+      mmap(nullptr, obj->size, PROT_READ | PROT_WRITE, MAP_SHARED, obj->fd, 0);
+  if (buf == MAP_FAILED) {
+    perror("mmap");
+    return false;
+  }
+  obj->data = buf;
+  // sanity check
+  struct stat sb;
+  if (fstat(obj->fd, &sb) == -1) {
+    perror("fstat");
+    return false;
+  }
+  assert(sb.st_size == obj->size);
+  printf("initialized fd %d of size %d\n", obj->fd, obj->size);
+  return true;
+}
+
+bool deinit_MemMap(MemMap *obj) {
+  puts("de-init memory map");
+  // The region is automatically unmapped when the process is terminated.
+  // But I will unmap it anyway for clarity (and also catch any bugs)
+  if (munmap(obj->data, obj->size) == -1) {
+    perror("munmap");
+    return false;
+  };
+  return true;
+}
+
+bool init_ShmObj(ShmObj *obj) {
+  puts("init shared memory object");
   auto fd = shm_open(obj->name, O_CREAT | O_RDWR, 0600);
   if (fd == -1) {
     perror("shm_open");
@@ -29,11 +76,13 @@ bool init(ShmObj *obj) {
     perror("ftruncate");
     return false;
   }
+  printf("initialized shmem obj %s, fd %d, expected size %d\n", obj->name,
+         obj->fd, obj->size);
   return true;
 }
 
-bool deinit(ShmObj *obj) {
-  puts("tearDown");
+bool deinit_ShmObj(ShmObj *obj) {
+  puts("de-init shared memory object");
   // could be closed earlier, right after mmap call
   if (close(obj->fd) == -1) {
     perror("close");
@@ -46,11 +95,14 @@ bool deinit(ShmObj *obj) {
   return true;
 }
 
-int parent(pid_t childPid, ShmObj *obj) {
-  defer deinit(obj);
+int parent(pid_t childPid, ShmObj *obj, MemMap *map) {
   printf("Parent. The child is %d\n", childPid);
+  defer deinit(obj);
   int wstatus;
   do {
+    // even if we do not wait for the child, it won't become zombie for long,
+    // because a zombie record in the process table is removed after the parent
+    // terminates (just a note)
     auto w = waitpid(childPid, &wstatus, 0);
     if (w == -1) {
       perror("waitpid");
@@ -60,15 +112,29 @@ int parent(pid_t childPid, ShmObj *obj) {
       printf("Exited, status=%d\n", WEXITSTATUS(wstatus));
     }
   } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+  // no need for mapping until the child has not finished, because the
+  // underlying shared memory object has a name, it will still be available.
+  if (!init(map)) {
+    return EXIT_FAILURE;
+  }
+  const char *message = map->data;
+  assert(strnlen(message, map->size) < map->size);
+  printf("MESSAGE: %s\n", message);
+  defer deinit(map);
   return EXIT_SUCCESS;
 }
 
-int child(ShmObj *obj) {
+int child(ShmObj *obj, MemMap *map) {
   puts("child");
-  if (close(obj->fd) == -1) {
-    perror("close");
+  defer close(obj->fd);
+  defer fflush(stdout);
+  if (!init(map)) {
+    return EXIT_FAILURE;
   }
-  fflush(stdout);
+  const char *message = "1234567";
+  assert(strlen(message) < map->size);
+  memcpy(map->data, message, map->size);
+  defer deinit(map);
   return EXIT_SUCCESS;
 }
 
@@ -81,14 +147,22 @@ int main() {
   if (!init(&shmObj)) {
     return EXIT_FAILURE;
   }
+  // The  child  process and the parent process run in separate memory spaces.
+  // At the time of fork() both memory spaces have the same  content.   Memory
+  // writes,  file  mappings (mmap(2)),  and unmappings (munmap(2)) performed by
+  // one of the processes do not affect the other.
   auto pid = fork();
   if (pid < 0) {
     perror("fork");
     return EXIT_FAILURE;
   }
-
+  MemMap memMap = {
+      .fd = shmObj.fd,
+      .data = nullptr,
+      .size = 8,
+  };
   if (pid == 0) {
-    return child(&shmObj);
+    return child(&shmObj, &memMap);
   }
-  return parent(pid, &shmObj);
+  return parent(pid, &shmObj, &memMap);
 }
